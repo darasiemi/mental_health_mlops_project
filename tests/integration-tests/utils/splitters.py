c1c6f9074443798c20661f9ac90c7e70ee0d0c83
Ubuntu <adedejioluwadara7@gmail.com>
2024-07-24 17:35:54 +0000

    integration test with localstark to test lambda and kinesis

---
+++ tests/integration-tests/utils/splitters.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def split_data(df):
+    y = df['label']
+    numerical_columns = ['lex_liwc_Tone',
+            'lex_liwc_negemo',
+            'lex_liwc_i',
+            'lex_liwc_Clout',
+            'sentiment']
+    categorical_columns = ['text']
+    X = df[numerical_columns + categorical_columns]
+
+    x_train,x_test,y_train,y_test=train_test_split(X,y,random_state=43)
+
+    return x_train,x_test,y_train,y_test
